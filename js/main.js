53aab309125dd95b44e20490e50e919197731228
Рябов <dmitryabov2017@yandex.ru>
2019-10-16 17:37:57 +0300

    добавил рабмку и счет

--- js/main.js
+++ js/main.js
@@ -4,3 +4,51 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var width = canvas.width;
 var height = canvas.height;
+var blockSize = 10;
+var widthInBlocks = width / blockSize;
+var heightInBlocks = height / blockSize;
+var score = 0;
+
+
+var scopePoint = function () {
+    score += 1;
+};
+
+
+
+var drawBorder = function () {
+  ctx.fillStyle = "Gray";
+  ctx.fillRect(0, 0, width, blockSize);
+  ctx.fillRect(0, height - blockSize, width, blockSize);
+  ctx.fillRect(0, 0, blockSize, height);
+  ctx.fillRect(width - blockSize, 0, blockSize, height);
+};
+
+
+var drawScore = function () {
+  ctx.font = "20px Courier";
+  ctx.fillStyle = "Black";
+  ctx.textAlign = "left";
+  ctx.textBaseline = "top";
+  ctx.fillText("Счет: " + score, blockSize, blockSize);
+};
+
+setInterval(function() {
+    ctx.clearRect(10, 10, width - 50, height - 50);
+    scopePoint();
+    drawScore();
+}, 1000);
+
+
+var gameOver = function () {
+  ctx.font = "60px Courier";
+  ctx.fillStyle = "Black";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText("Конец игры", width / 2, height / 2);
+};
+
+
+drawBorder();
+drawScore();
+
